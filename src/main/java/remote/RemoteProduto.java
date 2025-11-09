06238f7c0ca467fa70e9df889df93ccabc33e56a
GabrielaBonatelliPrates <gabriela.bonatelli06@gmail.com>
2025-11-09 17:37:38 -0300

    feat: Adição de interface "RemoteProduto".

---
+++ src/main/java/remote/RemoteProduto.java
@@ -0,0 +1,45 @@
+
+package remote;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface RemoteProduto {
+    void cadastrarProduto(String nomeProduto, double precoUnit, String unidadeProduto, int quantidadeEstoque,
+            int estoqueMinimo, int estoqueMaximo, String nomeCategoria, String tamanho, String embalagem)
+            throws RemoteException;
+
+    boolean atualizarProdutoBD(int idProduto, String nomeProduto, double precoUnit, String unidadeProduto, int quantidadeEstoque,
+            int estoqueMinimo, int estoqueMaximo, String nomeCategoria, String tamanho, String embalagem)
+            throws RemoteException;
+
+    boolean deletarProdutoBD(int idProduto)
+            throws RemoteException;
+
+    Produto buscarPorId(int idProduto)
+            throws RemoteException;
+
+    Produto buscarPorNome(String nomePesquisado)
+            throws RemoteException;
+
+    List<Produto> pegarProdutos()
+            throws RemoteException;
+
+    List<Produto> produtosOrdemAlfabética()
+            throws RemoteException;
+
+    List<Produto> pegarProdutosAbaixoMinimo()
+            throws RemoteException;
+
+    List<Produto> pegarProdutosAcimaMaximo()
+            throws RemoteException;
+
+    double valorTotal()
+            throws RemoteException;
+
+    String fichaProduto(String nomePesquisado)
+            throws RemoteException;
+
+    String verificaProduto(String nomePesquisado)
+            throws RemoteException;
+}
