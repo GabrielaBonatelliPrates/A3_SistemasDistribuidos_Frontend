0d33182227bf9ba15b56ad0244438f4a684f4907
Davi-Wolff <daviwolffdeabreu@gmail.com>
2025-11-10 15:24:46 -0300

    Feat: Adding RemoteMovimento

---
+++ src/main/java/remote/RemoteMovimento.java
@@ -0,0 +1,25 @@
+package remote;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Davi-Wolff
+ */
+public interface RemoteMovimento extends Remote{
+    
+    public void cadastraMovimentacao(MovimentacaoEstoque movimentacao) throws SQLException,RemoteException;
+    
+    public void inserirMovimentacao(int idProduto, Date data, int quantidadeMovimentada, String nomeProduto, String tipoMovimentacao) throws SQLException,RemoteException;
+    
+    public List<MovimentacaoEstoque> listarProdutosMovimentados() throws SQLException,RemoteException;
+
+    public void adicionarQuantidade(int idProduto, int quantidadeAdicionar)throws RemoteException;
+
+    public void retirarQuantidade(int idProduto, int quantidadeRetirar)throws RemoteException;
+    
+}
