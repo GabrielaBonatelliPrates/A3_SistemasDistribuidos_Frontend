a52d6bb06e82d933a2e991e4e499dca8cae12b19
Davi-Wolff <daviwolffdeabreu@gmail.com>
2025-11-11 17:46:12 -0300

    Feat: Adicionando RMIConnection

---
+++ src/main/java/rmiConnection/RMIConnection.java
@@ -0,0 +1,41 @@
+package rmiConnection;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import remote.RemoteCategoria;
+import remote.RemoteConexao;
+import remote.RemoteProduto;
+import remote.RemoteMovimento;
+
+public class RMIConnection {
+
+    private static RemoteProduto produto;
+    private static RemoteCategoria categoria;
+    private static RemoteMovimento movimentacao;
+
+    public static void connect(String host, int port) throws RemoteException, NotBoundException {
+        Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+        produto = (RemoteProduto) registry.lookup("ProdutoService");
+        categoria = (RemoteCategoria) registry.lookup("CategoriaService");
+        movimentacao = (RemoteMovimento) registry.lookup("MovimentacaoService");
+        RemoteConexao conexao = (RemoteConexao) registry.lookup("ConexaoService");
+    }
+
+    public static void connect(String host) throws RemoteException, NotBoundException {
+        connect("localhost", 1099);
+    }
+
+    public static RemoteProduto getProduto() {
+        return produto;
+    }
+
+    public static RemoteCategoria getCategoria() {
+        return categoria;
+    }
+
+    public static RemoteMovimento getMovimentacao() {
+        return movimentacao;
+    }
+}
