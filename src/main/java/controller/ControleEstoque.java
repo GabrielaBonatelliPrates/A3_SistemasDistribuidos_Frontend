48c2fdc319aa06ca8ec562f83a5c35c7d7bc105d
c3sarrr <cesar140904@gmail.com>
2025-11-13 15:13:48 -0300

    fix: Configurando Clase "ControleEstoque".

--- src/main/java/controller/ControleEstoque.java
+++ src/main/java/controller/ControleEstoque.java
@@ -1,41 +1,50 @@
 package controller;
 
-
-import dao.ProdutoDAO;
-import dao.CategoriaDAO;
-import dao.MovimentacaoDAO;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.sql.SQLException;
+import remote.RemoteCategoria;
+import remote.RemoteConexao;
+import remote.RemoteProduto;
 import view.FrmMenuPrincipal;
+import remote.RemoteMovimento;
+import rmiConnection.RMIConnection;
 
-/**classe ControleEstoque é uma classe utilizada para gerenciar a movimentação do estoque
+/**
+ * classe ControleEstoque é uma classe utilizada para gerenciar a movimentação
+ * do estoque
  *
  * @author laispaivaportela
  */
 public class ControleEstoque {
-    
 
-    /**metodos de instancia para efetivar o controle do estoque
+    /**
+     * metodos de instancia para efetivar o controle do estoque
      *
      * @param produtoDAO instancia um novo produto do banco de dados
      * @param categoriaDAO instancia uma nova categoria do banco de dados
      * @param movimentacaoDAO instancia uma nova movimentação do banco de dados
      */
-    protected CategoriaDAO categoriaDAO = new CategoriaDAO();
-    protected ProdutoDAO produtoDAO = new ProdutoDAO(categoriaDAO);
-    protected MovimentacaoDAO movimentacaoDAO = new MovimentacaoDAO(produtoDAO);
-
+    protected RemoteCategoria categoriaDAO;
+    protected RemoteProduto produtoDAO;
+    protected RemoteMovimento movimentacaoDAO;
+    protected RemoteConexao conexaoDAO;
 
     /**
-     * 
-     *@param iniciaAplicação metodo que inicia o sistema
+     *
+     * @param iniciaAplicação metodo que inicia o sistema
      */
-    public void iniciaAplicação() throws SQLException{
- 
+    public void iniciaAplicação() throws SQLException, RemoteException, NotBoundException {
+        RMIConnection.connect("localhost", 1099);
+        categoriaDAO = RMIConnection.getCategoria();
+        produtoDAO = RMIConnection.getProduto();
+        movimentacaoDAO = RMIConnection.getMovimentacao();
+        conexaoDAO = RMIConnection.getConexao();
         //instancia a interface gráfica do menu principal
-         FrmMenuPrincipal telaMenu = new FrmMenuPrincipal(produtoDAO, categoriaDAO, movimentacaoDAO);
-         //deixa ele visível
-         telaMenu.setVisible(true);
-         produtoDAO.pegarProdutos();
+        FrmMenuPrincipal telaMenu = new FrmMenuPrincipal(produtoDAO, categoriaDAO, movimentacaoDAO, conexaoDAO);
+        //deixa ele visível
+        telaMenu.setVisible(true);
+        produtoDAO.pegarProdutos();
     }
 
 }
