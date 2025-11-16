aa711e297d3d555a15b8d60cab9c9d9488e64d04
Davi-Wolff <daviwolffdeabreu@gmail.com>
2025-11-16 13:58:08 -0300

    feat: adicionando serializable no MovimentaçãoEstoque

--- src/main/java/model/MovimentacaoEstoque.java
+++ src/main/java/model/MovimentacaoEstoque.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -7,7 +8,7 @@ import java.util.Date;
  *
  * @author Davi-Wolff
  */
-public class MovimentacaoEstoque {
+public class MovimentacaoEstoque implements Serializable{
 
     /**
      * Declaração das variáveis de instância
